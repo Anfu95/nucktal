1a705be2a97eefbf915f830fa9ce7a059e7e49a1
Anfu95 <angeelfm18@gmail.com>
2022-05-18 17:21:01 -0500

    Create header and menu responsive

--- resources/views/livewire/navigation.blade.php
+++ resources/views/livewire/navigation.blade.php
@@ -1,19 +1,24 @@
-<header class="bg-trueGray-700">
-    <div class="container flex items-center h-16">
-        <a href=""
-            class="flex flex-col items-center justify-center px-4 bg-white bg-opacity-25 text-white cursor-pointer font-semibold h-full ">
-            <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
-                <path class="inline-flex" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                    d="M4 6h16M4 12h16M4 18h16" />
-            </svg>
-            <span>Categorías</span>
+<header class="bg-trueGray-700 sticky top-0" x-data="dropdown()">
+    <div class="container flex items-center h-16 justify-between md:justify-start">
+        <a 
+          :class="{'bg-opacity-100 text-orange-500' : open}"
+          x-on:click = "show()"
+          class="flex flex-col items-center justify-center order-last md:order-first px-6 md:px-4 bg-white bg-opacity-25 text-white cursor-pointer font-semibold h-full ">
+          <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+              <path class="inline-flex" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                  d="M4 6h16M4 12h16M4 18h16" />
+          </svg>
+          <span class="text-sm">Categorías</span>
         </a>
         <a href="/" class="mx-6">
             <x-jet-application-mark class="block h-9 w-auto" />
         </a>
-        @livewire('search')
 
-        <div class="mx-6 relative">
+        <div class="flex-1 hidden md:block">
+          @livewire('search')
+        </div>
+
+        <div class="mx-6 relative hidden md:block">
             @auth
                 <x-jet-dropdown align="right" width="48">
                     <x-slot name="trigger">
@@ -72,6 +77,103 @@
 
         </div>
 
-        @livewire('dropdown-cart')
+        <div class="hidden md:block">
+          @livewire('dropdown-cart')
+        </div>
     </div>
+
+    <nav id="navigation-menu"
+      :class="{'block': open, 'hidden': !open}"
+      class="bg-trueGray-700 bg-opacity-25 w-full absolute hidden">
+      {{--Menu computadora--}}
+      <div class="container h-full hidden md:block">
+        <div
+          x-show = "open"
+          x-on:click.away = "close()"
+          class="grid grid-cols-4 h-full relative">
+          <ul class="bg-white">
+            @foreach ($categories as $category)
+              <li class="navigation-link text-trueGray-500 hover:bg-orange-500 hover:text-white">
+                <a class="py-2 px-4 text-sm flex items-center" href="">
+                  <span class="flex justify-center w-9">
+                    {!!$category->icon!!}
+                  </span>
+                  {{$category->name}}
+                </a>
+                <div class="navigation-submenu bg-gray-100 absolute w-3/4 h-full top-0 right-0 hidden">
+                  <x-navigation-subcategories :category="$category"/>
+                </div>
+              </li>
+            @endforeach
+          </ul>
+          <div class="col-span-3 bg-gray-100">
+            <x-navigation-subcategories :category="$categories->first()"/>
+          </div>
+        </div>
+      </div>
+
+      {{--Menu computadora--}}
+      <div class="bg-white h-full overflow-y-auto">
+        <div class="container bg-gray-200 py-3 mb-2">
+          @livewire('search')
+        </div>
+        <ul>
+          @foreach ($categories as $category)
+            <li class="text-trueGray-500 hover:bg-orange-500 hover:text-white">
+              <a class="py-2 px-4 text-sm flex items-center" href="">
+                <span class="flex justify-center w-9">
+                  {!!$category->icon!!}
+                </span>
+                {{$category->name}}
+              </a>
+              <div class="navigation-submenu bg-gray-100 absolute w-3/4 h-full top-0 right-0 hidden">
+                <x-navigation-subcategories :category="$category"/>
+              </div>
+            </li>
+          @endforeach
+        </ul>
+        <p class="text-trueGray-500 px-6 my-2">USUARIOS</p>
+
+        @livewire('cart-mobile')
+
+        @auth
+          <a href="{{ route('profile.show') }}" 
+            class="py-2 px-4 text-sm flex items-center text-trueGray-500 hover:bg-orange-500 hover:text-white">
+            <span class="flex justify-center w-9">
+              <i class="far fa-address-card"></i>
+            </span>
+            Perfil
+          </a>
+          <a href=""
+            onclick="event.preventDefault();
+                    document.getElementById('logout-form').submit();"
+            class="py-2 px-4 text-sm flex items-center text-trueGray-500 hover:bg-orange-500 hover:text-white">
+            <span class="flex justify-center w-9">
+              <i class="fa fa-sign-out-alt"></i>
+            </span>
+            Cerrar sesión 
+          </a>
+          <form id="logout-form" action="{{route('logout')}}" method="post" class="hidden">
+            @csrf
+          </form>
+        @else
+          <a href="{{ route('login') }}" 
+            class="py-2 px-4 text-sm flex items-center text-trueGray-500 hover:bg-orange-500 hover:text-white">
+            <span class="flex justify-center w-9">
+              <i class="fas fa-user-circle"></i>
+            </span>
+            Inciar sesión
+          </a>
+          <a href="{{ route('register') }}"
+            onclick="event.preventDefault();
+                    document.getElementById('logout-form').submit();"
+            class="py-2 px-4 text-sm flex items-center text-trueGray-500 hover:bg-orange-500 hover:text-white">
+            <span class="flex justify-center w-9">
+              <i class="fas fa-fingerprint"></i>
+            </span>
+            Registrate 
+          </a>
+        @endauth
+      </div>
+    </nav>
 </header>
